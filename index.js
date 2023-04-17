2758e9e0a7c549a07263b7b30edbcca1f13474e3
snrp2002 <snrp2002@gmail.com>
2023-04-17 12:18:31 +0530

    modify server

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 import express from "express";
 import bodyParser from "body-parser";
-import {PythonShell} from 'python-shell';
+import {spawn} from 'child_process';
 import cors from 'cors';
 const PORT = process.env.PORT || 5000;
-
+import fs from 'fs';
 const app = express();
 app.use(bodyParser.urlencoded({limit: '30mb',extended: true}));
 app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(cors());
 app.post('/', async (req, res)=>{
-    const data = req.body.file;
+    const encoded_b64 = req.body.file;
     try{
-        const pyshell = new PythonShell('index.py');
-        pyshell.send(data);
-        pyshell.on('message', (message) => {
-            res.status(200).json({output: message});
+        const base64Data = encoded_b64.split(',')[1];
+        const decodedData = Buffer.from(base64Data, 'base64');
+        fs.writeFile('image.jpg', decodedData, (err) => {
+            if (err) throw err;
+        });
+        const pythonProcess = spawn('python', ['./index.py']);
+        pythonProcess.stdout.on('data', (data) => {
+            res.status(200).json({output: data.toString()});
+        });
+        pythonProcess.stderr.on('data', (data) => {
+            throw new Error(data);
         });
     }catch(err){
         res.status(400).json({message: err.message});
--- index.py
+++ index.py
@@ -1,17 +1,17 @@
 import matplotlib.image as mpimg
 import joblib
 import cv2
-import base64
-
+# import base64
+# import sys
 try:
     loaded_model = joblib.load("model.pkl")
 
-    encoded_b64 = input()
-    decoded_b64= base64.b64decode(encoded_b64.split(',')[1])
+    # encoded_b64 = sys.argv[1]
+    # decoded_b64= base64.b64decode(encoded_b64.split(',')[1])
 
-    img_file = open('image.jpg', 'wb')
-    img_file.write(decoded_b64)
-    img_file.close()
+    # img_file = open('image.jpg', 'wb')
+    # img_file.write(decoded_b64)
+    # img_file.close()
 
     image = mpimg.imread('image.jpg')
 

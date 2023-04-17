398103f75c9d5e2a922def423b43ce43c03555e2
snrp2002 <snrp2002@gmail.com>
2023-04-17 21:34:20 +0530

    modify server - modify error handling

--- index.js
+++ index.js
@@ -8,23 +8,36 @@ const app = express();
 app.use(bodyParser.urlencoded({limit: '30mb',extended: true}));
 app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(cors());
-app.post('/', async (req, res)=>{
-    const encoded_b64 = req.body.file;
-    try{
-        const base64Data = encoded_b64.split(',')[1];
-        const decodedData = Buffer.from(base64Data, 'base64');
-        fs.writeFile('image.jpg', decodedData, (err) => {
-            if (err) throw err;
-        });
-        const pythonProcess = spawn('python', ['./index.py']);
-        pythonProcess.stdout.on('data', (data) => {
-            res.status(200).json({output: data.toString()});
-        });
-        pythonProcess.stderr.on('data', (data) => {
-            throw new Error(data.toString());
-        });
-    }catch(err){
-        res.status(400).json({message: err.message});
-    }
-})
+app.post('/', async (req, res) => {
+  const encoded_b64 = req.body.file;
+  try {
+    const base64Data = encoded_b64.split(',')[1];
+    const decodedData = Buffer.from(base64Data, 'base64');
+    await new Promise((resolve, reject) => {
+      fs.writeFile('image.jpg', decodedData, (err) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
+    const pythonProcess = spawn('python', ['./index.py']);
+    const output = await new Promise((resolve, reject) => {
+      let data = '';
+      pythonProcess.stdout.on('data', (chunk) => {
+        data += chunk;
+      });
+      pythonProcess.stderr.on('data', (err) => {
+        reject(err.toString());
+      });
+      pythonProcess.on('close', (code) => {
+        if (code !== 0) reject(`Python process exited with code ${code}`);
+        else resolve(data);
+      });
+    });
+    console.log(output);
+    res.status(200).json({ output: output });
+  } catch (err) {
+    res.status(400).json({ message: err});
+  }
+});
+
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
--- index.py
+++ index.py
@@ -1,18 +1,8 @@
 import matplotlib.image as mpimg
 import joblib
 import cv2
-# import base64
-# import sys
 try:
     loaded_model = joblib.load("model.pkl")
-
-    # encoded_b64 = sys.argv[1]
-    # decoded_b64= base64.b64decode(encoded_b64.split(',')[1])
-
-    # img_file = open('image.jpg', 'wb')
-    # img_file.write(decoded_b64)
-    # img_file.close()
-
     image = mpimg.imread('image.jpg')
 
     resized_image = cv2.resize(image, (28, 28), interpolation = cv2.INTER_LINEAR)
@@ -23,6 +13,6 @@ try:
     temp = temp/255
     result = loaded_model.predict(temp, verbose=False)
     result = result.argmax(axis=1)
-    print(result[0])
+    print(result[0], end='')
 except:
-    print("Something went wrong")
+    raise Exception("Error")
